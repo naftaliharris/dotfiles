7c10372d6ad7bf484ad776cfc7e10675b9baee09
Naftali Harris <naftaliharris@gmail.com>
2016-10-27 10:30:47 -0700

    Submodules -> copied code

---
+++ .vim/bundle/tskeleton_vim/skeletons/plain.java
@@ -0,0 +1,13 @@
+/** 
+ * <+FILE NAME+>
+ * Date: <+DATE+>
+ * Author: Naftali Harris
+ */
+
+public class <+FILE NAME ROOT+> {
+
+    public static void main(String[] args) {
+        
+    }
+
+}
---
+++ .vim/bundle/tskeleton_vim/skeletons/plain.py
@@ -0,0 +1,12 @@
+"""
+<+FILE NAME+>
+Date: <+DATE+>
+Author: Naftali Harris
+"""
+
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
