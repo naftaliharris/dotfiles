225028ce482a97eae8e2dfe100d7d7f1bc452ab0
Naftali Harris <naftaliharris@gmail.com>
2017-01-11 11:11:59 -0800

    Added an IPython config

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 # Run this to install your dotfiles.
 
+set -e
+
+
 BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 files=$(ls -A $BASEDIR | grep -v -x -f $BASEDIR/dont_link.txt)
@@ -13,3 +16,9 @@ do
     fi
     ln -s -f $BASEDIR/$file $HOME/$file
 done
+
+# Do this last so that if it fails (e.g. if IPython isn't installed), we've
+# still done the previous stuff.
+ipython create profile
+file=ipython_config.py
+ln -s -f $BASEDIR/$file $(ipython locate profile)/$file
